fe099a800c1256ebb12bfb010f750d600e29e0cf
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-07 19:54:03 +0200

    Update repository interface

--- UnitTests.pkrzysiekk/ShoppingListAPI/ShoppingListAPI/Repository/IRepository.cs
+++ UnitTests.pkrzysiekk/ShoppingListAPI/ShoppingListAPI/Repository/IRepository.cs
@@ -2,9 +2,10 @@ namespace ShoppingListAPI.Repository;
 
 public interface IRepository<T>
 {
-    public Task<T> GetById(int id);
-    public Task<IEnumerable<T>> GetAll();
+    public Task<T?> GetById(int id);
+    public IQueryable<T> GetAll();
     public Task Add(T item);
     public Task Update(T item);
     public Task Delete(T item);
+    public Task SaveChanges();
 }
