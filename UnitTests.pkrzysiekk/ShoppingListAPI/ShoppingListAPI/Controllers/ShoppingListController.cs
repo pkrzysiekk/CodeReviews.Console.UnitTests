3076061e9c7156cff980c2b608b32f2120bf1230
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-07 14:33:36 +0200

    Init the Backend project

---
+++ UnitTests.pkrzysiekk/ShoppingListAPI/ShoppingListAPI/Controllers/ShoppingListController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingListAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ShoppingListController : ControllerBase
+{
+
+
+
+}
